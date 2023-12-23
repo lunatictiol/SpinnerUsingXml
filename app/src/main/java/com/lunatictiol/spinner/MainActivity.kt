65a7e767a97e4b5cdbb70360bec86dafacf9ae05
Sabiq Aijaz <63781925+lunatictiol@users.noreply.github.com>
2023-12-23 18:15:52 +0530

    first commit project created

---
+++ app/src/main/java/com/lunatictiol/spinner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lunatictiol.spinner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
